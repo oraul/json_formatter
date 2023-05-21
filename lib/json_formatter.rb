1d0b223db08e7fed55e0c5df9d20da52a11aa0cf
oraul <1838418+oraul@users.noreply.github.com>
2023-05-21 19:34:39 -0300

    Treat message type

--- lib/json_formatter.rb
+++ lib/json_formatter.rb
@@ -1,12 +1,26 @@
+# frozen_string_literal: true
+
 require 'json'
 
 class JsonFormatter
   def call(severity, timestamp, progname, message)
     JSON.dump(
-      severity: severity,
-      timestamp: timestamp,
-      progname: progname,
-      message: message
+      {
+        severity: severity,
+        timestamp: timestamp,
+        progname: progname
+      }.merge!(treat(message))
     )
+  end
+
+  def treat(message)
+    case message
+    when String
+      { message: message }
+    when Hash
+      message
+    else
+      { message: message.inspect }
+    end
   end
 end
--- test/test_json_formatter.rb
+++ test/test_json_formatter.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require 'json_formatter'
 
 class JsonFormatterTest < Minitest::Test
-  def test_call
+  def test_string_message
     expected = {
       severity: 'INFO',
       timestamp: '2022-11-19T15:47:47-03:00',
@@ -11,6 +11,36 @@ class JsonFormatterTest < Minitest::Test
     }.to_json
 
     actual = JsonFormatter.new.call('INFO', '2022-11-19T15:47:47-03:00', 'test', 'Foo')
+
+    assert_equal(expected, actual)
+  end
+
+  def test_hash_message
+    expected = {
+      severity: 'INFO',
+      timestamp: '2022-11-19T15:47:47-03:00',
+      progname: 'test',
+      message: 'Foo',
+      custom: {
+        foo: 'bar'
+      }
+    }.to_json
+
+    actual = JsonFormatter.new.call('INFO', '2022-11-19T15:47:47-03:00', 'test', { message: 'Foo', custom: { foo: 'bar' } })
+
+    assert_equal(expected, actual)
+  end
+
+  def test_exception_message
+    expected = {
+      severity: 'INFO',
+      timestamp: '2022-11-19T15:47:47-03:00',
+      progname: 'test',
+      message: '#<StandardError: Foo>',
+    }.to_json
+
+    actual = JsonFormatter.new.call('INFO', '2022-11-19T15:47:47-03:00', 'test', StandardError.new('Foo'))
+
     assert_equal(expected, actual)
   end
 end
